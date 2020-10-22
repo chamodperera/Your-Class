fb4f314a21829b48f6fa2a2b87b5bc5eb648ffd3
chamodperera <chamodperera87@gmail.com>
2020-10-22 15:24:05 +0530

    added translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'YourClass';
+  public storedLang: string;
+
+  constructor(public translateService: TranslateService){
+    const browserLang = localStorage.getItem('lang');
+
+    if(browserLang=="sn"){
+      translateService.use('sn');
+      this.storedLang = 'sn';
+    }else if(browserLang=="tm"){
+      translateService.use('tm');
+      this.storedLang = 'tm';
+    }else{
+      translateService.use('en');
+      this.storedLang = 'en';
+    }
+  }
 }
