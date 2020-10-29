f66b44a28fe90dbd9de1cb9b25ed4553393c8447
STEdirisuriya <supunedirisuriya2002@gmail.com>
2020-10-29 13:39:35 +0530

    added gauth

--- src/app/in-window/profile/profile.component.ts
+++ src/app/in-window/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth) {
+  }
+  login() {
+    this.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.auth.signOut();
+  }
 
   ngOnInit(): void {
   }
