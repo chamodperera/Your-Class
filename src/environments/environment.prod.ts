3a6374cf72b3f1aacb14f92aa0b572a8b8d352eb
chamodperera <39716499+chamodperera@users.noreply.github.com>
2020-11-15 06:58:19 +0530

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCkc84phWuurfjAH2DT31WeO-ct-k9sLvM",
+    authDomain: "your-class-2020.firebaseapp.com",
+    databaseURL: "https://your-class-2020.firebaseio.com",
+    projectId: "your-class-2020",
+    storageBucket: "your-class-2020.appspot.com",
+    messagingSenderId: "44650067995",
+    appId: "1:44650067995:web:245356275487c9f4ae20be",
+    measurementId: "G-5LP3KREMTN"
+  }
 };
